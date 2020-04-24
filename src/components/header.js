33808f1945b751712b1749ac85938f536d54fac6
Aniket kumar sharma <sharma.ani.4298@gmail.com>
2020-04-24 14:55:07 +0530

    install helemt and worked with graphQl

--- src/components/header.js
+++ src/components/header.js
@@ -6,17 +6,14 @@ import { Link } from 'gatsby';
 const NavLink = styled(Link)`
   color: #222;
   font-size: 1rem;
-  /* font-weight: ${props => props.fontWeight || 'normal'}; */
-  font-weight: 'normal';
+  font-weight: ${props => props.fontWeight || 'normal'};
   line-height: 1;
   margin: 0 0.5rem 0 0;
   padding: 0.25rem;
   text-decoration: none;
-
   &.current-page {
     border-bottom: 2px solid #222;
   }
-
   &:last-of-type {
     margin-right: 0;
   }
